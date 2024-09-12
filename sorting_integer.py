c6dc4c80814960b3f5818cd442b09fa644e1ec54
= <=>
2024-09-12 16:28:43 -0600

    comment and clean up counting_sort

--- sorting_integer.py
+++ sorting_integer.py
@@ -4,34 +4,35 @@
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    Running time: Real world: 0.0013s
+        Time Complexity: O(n) + O(n) + O(n + m) -> O(n + m), where n is the length of the list and m is the
+        size of the range of values
+    Memory usage:
+        O(m) - we create a list to store the counts (of size m), but copy the output list in place
+    """
     max_number = numbers[0]
     min_number = numbers[0]
 
     # find min_number and max_number, O(n)
-    for _, item in enumerate(numbers):
+    for item in numbers:
         if item < min_number:
             min_number = item
         if item > max_number:
             max_number = item
+
+    # build the count list, O(m) where m is the range between min and max
     counts_list = [0] * (max_number - min_number + 1)
-    print(counts_list)
-    print(min_number)
-    print(max_number)
+
     # determine counts, O(n)
-    for _, item in enumerate(numbers):
-        print(item)
+    for item in numbers:
         counts_list[item - min_number] += 1
 
     i = 0
     j = 0
+    # Outer loop is O(m)
     while i < len(counts_list):
+        #if the count is > 0, we need to copy another of that value to index j in our output list
+        # this loop runs n times total across all iterations - so these nested loops are O(n + m)
         while counts_list[i] != 0:
             numbers[j] = i + min_number
             j += 1
